6642bb20ee2ce729142cab73d1f5cd121b5d7b36
unknown <andrelabanca2@gmail.com>
2021-07-27 18:28:29 -0300

    feat: criação dos arquivos do banco sqlite e configurações iniciais.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Olá nlw");
-})
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá nlw método post");
-})
+import "./database"
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running nlw"));
