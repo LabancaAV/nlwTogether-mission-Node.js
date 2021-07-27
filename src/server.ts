0fbfc860d97c093caef6766e2c8b2f7d43937094
unknown <andrelabanca2@gmail.com>
2021-07-26 21:40:40 -0300

    feat: criação da rota get e post para testar a conexão.

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/test", (request, response) => {
+    return response.send("Olá nlw");
+})
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá nlw método post");
+})
+
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running nlw"));
