f602ead243e14ed6b81e2fed465c7eb35f22bfef
unknown <andrelabanca2@gmail.com>
2021-07-31 15:04:28 -0300

    feat: criação da service CreateComplimentService.ts da tabela compliments.

---
+++ src/services/CreateComplimentService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../retositories/ComplimentRepositories"
+import { UserRepositories } from "../retositories/UserRepositories";
+
+
+interface IComplimentRequest{
+    tag_id: string;
+    user_sender: string;
+    user_receiver: string;
+    message: string;
+}
+
+class CreateComplimentService {
+    async execute( { tag_id, user_sender, user_receiver, message }: IComplimentRequest) {
+        const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+        const usersRepositories = getCustomRepository(UserRepositories);
+
+        if(user_sender === user_receiver){
+            throw new Error("Incorrect User Receiver");
+        }
+
+        const userReceiverExists = await usersRepositories.findOne(user_receiver);
+
+        if(!userReceiverExists){
+            throw new Error("User Receiver does not exists!");
+        }
+
+        const compliment = complimentsRepositories.create({
+            tag_id,
+            user_receiver,
+            user_sender,
+            message
+        });
+
+        await complimentsRepositories.save(compliment);
+
+        return compliment;
+
+    }
+}
+
+export { CreateComplimentService }
