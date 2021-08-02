ad68d9ee434f160e150074e42038f2aa9b9d93ff
unknown <andrelabanca2@gmail.com>
2021-08-02 19:37:17 -0300

    refactor: modificação do middleware ensureAdmin para verificar se o usuário é admin.

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,14 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../retositories/UserRepositories";
 
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction){
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction){
 
     const { user_id } = request;
     console.log(user_id);
 
-    // verificar se usuário admin
-    const admin = true;
 
+    const usersRepositories = getCustomRepository(UserRepositories);
+
+    const { admin } = await usersRepositories.findOne(user_id);
+
+    // verificar se usuário admin
+    
     if(admin){
         return next();
     }
