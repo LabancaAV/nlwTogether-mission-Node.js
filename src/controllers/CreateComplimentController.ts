5f7832f065acd75823e107178c6490f0da2f1680
unknown <andrelabanca2@gmail.com>
2021-08-02 20:22:00 -0300

    refactor: modificação da forma como recebemos o id do usuário logado. Agora recebemos pelo request do token.

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -1,16 +1,17 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentController {
    
     async handle(request: Request, response: Response){
-        const { tag_id, user_sender, user_receiver, message } = request.body;
+        const { tag_id, user_receiver, message } = request.body;
+        const { user_id } = request;
 
         const createComplimentService = new CreateComplimentService();
 
         const compliment = await createComplimentService.execute({
             tag_id, 
-            user_sender, 
+            user_sender: user_id, 
             user_receiver, 
             message
         });
