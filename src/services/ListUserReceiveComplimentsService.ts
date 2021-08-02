51e3db5f64dddcfed906c7b1c53cc8adb8e50da0
unknown <andrelabanca2@gmail.com>
2021-08-02 20:33:29 -0300

    feat: criação das services ListUserReceiveComplimentsService.ts e ListUserSendComplimentsService.ts .

---
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../retositories/ComplimentRepositories";
+
+class ListUserReceiveComplimentsService {
+
+    async execute(user_id: string){
+        const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+
+        const compliments = await complimentsRepositories.find({
+            where: {
+                user_receiver: user_id
+            }
+        })
+
+        return compliments;
+    }
+}
+
+export { ListUserReceiveComplimentsService }
